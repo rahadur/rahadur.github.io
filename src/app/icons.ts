c8947b81308aa97a32b488ec2332e8a9e469611a
Rahadur Rahman <rahadur.rahman@brainstation-23.com>
2023-09-01 14:27:53 +0600

    add social profile links

--- src/app/icons.ts
+++ src/app/icons.ts
@@ -1,5 +1,11 @@
 import { menuIcon } from '@svg/menu';
+import { linkedinIcon } from '@svg/linkedin';
+import { githubIcon } from '@svg/github';
+import { stackoverflowIcon } from '@svg/stackoverflow';
 
 export const icons = [
-  menuIcon
+  menuIcon,
+  linkedinIcon,
+  githubIcon,
+  stackoverflowIcon
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { SvgIconComponent } from '@ngneat/svg-icon';
+
+type SocialPlatform = 'linkedin' | 'github' | 'stackoverflow';
+enum SocialPlatformUrl  {
+  Linkedin = 'https://www.linkedin.com/in/rahadur/',
+  Github = 'https://github.com/rahadur',
+  StackOverflow = 'https://stackoverflow.com/users/5290908/rahadur'
+}
 
 @Component({
   standalone: true,
   selector: 'home-page',
   templateUrl: 'home.component.html',
+  imports: [SvgIconComponent],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  openSocialLink(platform: SocialPlatform): void {
+    switch (platform) {
+      case 'github':
+        window.location.href = SocialPlatformUrl.Github;
+        break;
+      case 'stackoverflow':
+        window.location.href = SocialPlatformUrl.StackOverflow;
+        break;
+      default:
+        window.location.href = SocialPlatformUrl.Linkedin;
+        break;
+    }
+  }
 }
